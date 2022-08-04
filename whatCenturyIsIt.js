0a9252a65a7d5165e29924852da78d37f35868d8
theo-wall <theo.wall@propra.ca>
2022-08-04 15:11:20 -0600

    solved

--- whatCenturyIsIt.js
+++ whatCenturyIsIt.js
@@ -7,7 +7,21 @@
 
 const numbers = [1999, 2011, 2154, 2259, 1124, 2000];
 
-const whatCentury = (num) => {};
+const whatCentury = (num) => {
+  let century = Math.ceil(num / 100);
+  let j = century % 10;
+  let k = century % 100;
+  if (j == 1 && k != 11) {
+    return century.toString() + "st";
+  }
+  if (j == 2 && k != 12) {
+    return century.toString() + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return century.toString() + "rd";
+  }
+  return century.toString() + "th";
+};
 
 const answers = numbers.map((num) => {
   return whatCentury(num);
