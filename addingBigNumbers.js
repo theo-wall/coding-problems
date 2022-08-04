dd0af7f494861a370c54e16a0a7d8ba4ca51c3e0
theo-wall <theo.wall@propra.ca>
2022-08-04 15:27:56 -0600

    solved

--- addingBigNumbers.js
+++ addingBigNumbers.js
@@ -8,7 +8,24 @@
 // fix me!
 
 const add = (a, b) => {
-  return (Number(a) + Number(b)).toString();
+  const AL = a.length;
+  const BL = b.length;
+  const ML = Math.max(AL, BL);
+
+  let carry = 0,
+    sum = "";
+
+  for (let i = 1; i <= ML; i++) {
+    let A = +a.charAt(AL - i);
+    let B = +b.charAt(BL - i);
+
+    let t = carry + A + B;
+    carry = (t / 10) | 0;
+    t %= 10;
+
+    sum = i === ML && carry ? carry * 10 + t + sum : t + sum;
+  }
+  return sum;
 };
 
 const answer = add(
