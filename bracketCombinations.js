9c4c73a5a8bd73b53b759473c0cfcb01827f714e
theo-wall <theo.wall11@gmail.com>
2022-08-10 15:44:16 -0600

    setup

---
+++ bracketCombinations.js
@@ -0,0 +1,24 @@
+// Bracket Combinations
+// Have the function BracketCombinations(num) read num which will be an integer greater than or equal to zero,
+// and return the number of valid combinations that can be formed with num pairs of parentheses. For example,
+// if the input is 3, then the possible combinations of 3 pairs of parenthesis,
+// namely: ()()(), are ()()(), ()(()), (())(), ((())), and (()()).
+// There are 5 total combinations when the input is 3, so your program should return 5.
+
+// Examples
+// Input: 3
+// Output: 5
+// Input: 2
+// Output: 2
+
+const numbers = [2, 3];
+
+const bracketCombinations = (num) => {
+  return num;
+};
+
+const answers = numbers.map((num) => {
+  return bracketCombinations(num);
+});
+
+console.log(answers);
